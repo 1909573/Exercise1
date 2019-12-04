02185e502edf299e8e577ed3eb0c3fbff3d3ae9a
User <cshong199@hotmail.com>
2019-12-04 11:30:20 +0800

    excerise 1

--- app/src/main/java/com/example/exercise1/MainActivity.kt
+++ app/src/main/java/com/example/exercise1/MainActivity.kt
@@ -10,6 +10,7 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import org.w3c.dom.Text
 
@@ -39,34 +40,52 @@ class MainActivity : AppCompatActivity() {
     }
 
     @SuppressLint("SetTextI18n")
-    private fun calculatePrice(view:View){
-        //input from users
-        val carPrice = findViewById<EditText>(R.id.editTextCarPrice)
-        val downPayment = findViewById<EditText>(R.id.editTextDownPayment)
-        val loanPeriod = findViewById<EditText>(R.id.editTextLoanPeriod)
-        val interestRate = findViewById<EditText>(R.id.editTextInterestRate)
+    private fun calculatePrice(view:View) {
 
 
+        val alertBox = AlertDialog.Builder(this@MainActivity)
 
-        //Algorithm
+        alertBox.setTitle("Error")
+        alertBox.setMessage("Please fill in the empty space")
+        alertBox.setIcon(R.mipmap.ic_launcher)
 
-        val carLoan =  carPrice.text.toString().toInt() -  downPayment.text.toString().toInt()
-        val interest = carLoan * (interestRate.text.toString().toDouble()/100) * loanPeriod.text.toString().toInt()
-        val monthlyPay = (carLoan + interest) /  loanPeriod.text.toString().toInt() / 12
+        alertBox.setNegativeButton("CLOSE") { dialog, which ->
+            dialog.dismiss()
+        }
 
 
+        if (editTextCarPrice.text.length == 0 || editTextDownPayment.text.length == 0 || editTextLoanPeriod.text.length == 0 || editTextInterestRate.text.length == 0) {
 
-        //result Text
-        val resultText1 = findViewById<TextView>(R.id.textViewLoan)
-        val resultText2 = findViewById<TextView>(R.id.textViewInterest)
-        val resultText3 = findViewById<TextView>(R.id.textViewMonthlyRepayment)
+            alertBox.show()
+        } else {
+            //input from users
+            val carPrice = findViewById<EditText>(R.id.editTextCarPrice)
+            val downPayment = findViewById<EditText>(R.id.editTextDownPayment)
+            val loanPeriod = findViewById<EditText>(R.id.editTextLoanPeriod)
+            val interestRate = findViewById<EditText>(R.id.editTextInterestRate)
 
-        resultText1.text = "Loan : " + carLoan.toString()
-        resultText2.text = "Interest : " + interest
-        resultText3.text = "Monthly Repayment : " + monthlyPay
 
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            //Algorithm
+
+            val carLoan = carPrice.text.toString().toInt() - downPayment.text.toString().toInt()
+            val interest =
+                carLoan * (interestRate.text.toString().toDouble() / 100) * loanPeriod.text.toString().toInt()
+            val monthlyPay = (carLoan + interest) / loanPeriod.text.toString().toInt() / 12
+
+
+            //result Text
+            val resultText1 = findViewById<TextView>(R.id.textViewLoan)
+            val resultText2 = findViewById<TextView>(R.id.textViewInterest)
+            val resultText3 = findViewById<TextView>(R.id.textViewMonthlyRepayment)
+
+            resultText1.text = "Loan : " + carLoan.toString()
+            resultText2.text = "Interest : " + interest
+            resultText3.text = "Monthly Repayment : " + monthlyPay
+
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 
 
